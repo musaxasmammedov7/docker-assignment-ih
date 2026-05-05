c5bc10c527c670542fee3d9a90a923de8138ccc6
musaxasmammedov7 <musaxasmammedov7@icloud.com>
2026-05-05 19:22:16 +0400

    number5

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
